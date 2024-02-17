72fdabf9cc49747d8250271e1649e80b54e129f0
Yago López Losada <yago@MacBook-de-yago.local>
2024-02-17 15:00:13 +0100

    Pequeños cambios en comentarios

--- src/components/Carrusel/SlideCarrusel.js
+++ src/components/Carrusel/SlideCarrusel.js
@@ -1,7 +1,7 @@
 
 function SlideCarrusel ({ imagen, alt, estado, descripcion, titulo }) {
     return (
-        <div className={estado} data-bs-interval="4000">
+        <div className={estado} data-bs-interval="3000">
             <img src= {imagen} className="d-block w-100" alt={alt} />
             <div className="carousel-caption d-none d-md-block">
                 <h5>{titulo}</h5>
